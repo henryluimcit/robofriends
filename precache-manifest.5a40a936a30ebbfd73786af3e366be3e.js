2572d7354b861dec041a1cea87d43266dc03ecb2
Henry Lui <henrykinsunl@gmail.com>
2020-01-06 15:31:24 -0600

    Updates

---
+++ precache-manifest.5a40a936a30ebbfd73786af3e366be3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3bda5321ccc65bb20765b6464c2893",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "09bc4a51c45092bbbfda",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0eef0fea1285670dc075",
+    "url": "/robofriends/static/css/main.cd26967a.chunk.css"
+  },
+  {
+    "revision": "09bc4a51c45092bbbfda",
+    "url": "/robofriends/static/js/2.8bace378.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8bace378.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0eef0fea1285670dc075",
+    "url": "/robofriends/static/js/main.8d69697c.chunk.js"
+  },
+  {
+    "revision": "3d76cb14cea2be39c99c",
+    "url": "/robofriends/static/js/runtime-main.525ce95a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
